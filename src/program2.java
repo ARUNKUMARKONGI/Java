fae62ac3b71c61391e6ce3c465bf1b152f5ae753
kumararunKongi <100160495+kumararunKongi@users.noreply.github.com>
2023-08-18 12:41:00 +0530

    Successfully updated

--- src/program2.java
+++ src/program2.java
@@ -1,6 +1,6 @@
 public class program2 {
     public static void main(String[]args)
     {
-        System.out.println("this is second program");
+        System.out.println("this is second program updated");
     }
 }
