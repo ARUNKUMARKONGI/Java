e92170453c78ddaf37740d9fa81042f5d0f38c24
ARUNKUMARKONGI <37120209+ARUNKUMARKONGI@users.noreply.github.com>
2023-08-18 16:01:56 +0530

    Update program1.java

--- src/program1.java
+++ src/program1.java
@@ -1,7 +1,7 @@
 public class program1 {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
     }
