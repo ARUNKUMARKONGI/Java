53e63220f3ca08a0aee618d48f34e2ef7afb2b03
ARUNKUMARKONGI <arunkongi@gmail.com>
2023-09-04 10:30:25 +0530

    Successfully updated

--- src/program2.java
+++ src/program2.java
@@ -1,11 +1,11 @@
 public class program2 {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         int a = 10;
 //        intellij smart suggestions, remove = and right click to see suggestions
         if (a == 10) {
-            System.out.println("Value of a is "+a);
+            System.out.println("Value of a is " + a);
         }
-        if(a==10) {
+        if (a == 10) {
             System.out.println("hello");
         }
     }
--- src/sample.java
+++ src/sample.java
@@ -1,6 +1,6 @@
 public class sample {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!\n");
+        System.out.printf("Hello and welcome\n " + args[0] + "\n");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
