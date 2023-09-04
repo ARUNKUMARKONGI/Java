10439651cfbb0ae2c30847730270688bd3bc53b5
ARUNKUMARKONGI <arunkongi@gmail.com>
2023-09-04 10:42:42 +0530

    Successfully updated

--- src/sample.java
+++ src/sample.java
@@ -1,7 +1,7 @@
 public class sample {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome\n " + args[0] + "\n");
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 20; i++) {
             System.out.println("i = " + i);
         }
     }
