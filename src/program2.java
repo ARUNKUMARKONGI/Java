ebe955de9eb41cdccf8ae6992f2b2ad7621b46ed
ARUNKUMARKONGI <arunkongi@gmail.com>
2023-10-13 00:13:28 +0530

    Successfully updated

--- src/program2.java
+++ src/program2.java
@@ -2,7 +2,7 @@ public class program2 {
     public static void main(String[] args) {
         int a = 10;
 //        intellij smart suggestions, remove = and right click to see suggestions
-        if (a == 100) {
+        if (a == 10) {
             System.out.println("Value of a is " + a);
         }
         if (a == 10) {
