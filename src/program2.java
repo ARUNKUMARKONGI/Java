8ac105022860d0ec8e92ac14e9361496112f0c56
ARUNKUMARKONGI <37120209+ARUNKUMARKONGI@users.noreply.github.com>
2023-10-13 00:23:23 +0530

    Update program2.java

--- src/program2.java
+++ src/program2.java
@@ -3,7 +3,7 @@ public class program2 {
         int a = 10;
 //        intellij smart suggestions, remove = and right click to see suggestions
         if (a == 10) {
-            System.out.println("Value of a is av vccfcrunsdgdbfhv" + a);
+            System.out.println("Value of a is" + a);
         }
        /* if (a == 10) {
             System.out.println("hello");
